d5dc2667f59c90f37291a00f096127d41766242e
Ryan77777777 <116934407+Ryan77777777@users.noreply.github.com>
2022-10-29 15:50:24 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+pip install flask
+
+
+# In[7]:
+
+
+from flask import Flask
+import joblib
+
+
+# In[8]:
+
+
+app = Flask(__name__)
+
+
+# In[9]:
+
+
+@app.route("/",)
+
+
+# In[ ]:
+
+
+
+
